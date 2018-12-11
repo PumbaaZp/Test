2b81f1dc15fa9977939caecca79a7e6ae8723fde
zhangp <zhangp@linkcld.com>
2018-12-11 13:45:28 +0800

    修改线程池全部完成的判断方式

--- src/com/pro/thread/TestExecutors.java
+++ src/com/pro/thread/TestExecutors.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description 测试线程池
@@ -14,18 +15,30 @@ import java.util.concurrent.Executors;
 public class TestExecutors {
     private List<String> onlineThread = Lists.newArrayList();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         TestExecutors testExecutors = new TestExecutors();
         testExecutors.startThreadPoll();
     }
 
-    public void startThreadPoll() throws InterruptedException {
+    public void startThreadPoll(){
         int size = 20;
         ExecutorService executorService = Executors.newFixedThreadPool(size);
         for(int i=0;i<size;i++){
             executorService.execute(new ValidateThread(i));
         }
         executorService.shutdown();
+        try{
+            if(!executorService.awaitTermination(10, TimeUnit.SECONDS)){
+                System.out.println("Threads overtime exist. Still " + onlineThread.size() + " thread");
+                onlineThread.clear();
+                executorService.shutdownNow();
+            }else{
+                System.out.println("Threads complete, size: " + size);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Catch interrupt exception");
+        }
+        /*long statTime = System.currentTimeMillis();
         while (true){
             if(executorService.isTerminated()){
                 System.out.println("Threads end;");
@@ -34,7 +47,13 @@ public class TestExecutors {
                 System.out.println("Threads contains " + onlineThread.size() + " thread");
                 Thread.sleep(200);
             }
-        }
+            if(System.currentTimeMillis() - statTime > 10 * 1000){
+                System.out.println("Threads overtime exist. Still " + onlineThread.size() + " thread");
+                executorService.shutdownNow();
+                onlineThread.clear();
+                break;
+            }
+        }*/
     }
 
     class ValidateThread extends Thread{
@@ -47,11 +66,15 @@ public class TestExecutors {
 
         public void run(){
             onlineThread.add(String.valueOf(num));
-            if(num % 5 == 0){
-                System.out.println(num / 0);
-            }else{
-                System.out.println(num);
+            try {
+                Thread.sleep(num * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            if(num % 5 == 0){
+            }else{
+            }
+            System.out.println("Thread num: " + num + " occur");
             onlineThread.remove(String.valueOf(num));
         }
     }
